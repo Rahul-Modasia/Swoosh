bc7f09239874e4f1d04f3d2b7dc97a9a41636cce
unknown <nkmodasia@gmail.com>
2018-06-02 16:18:33 +0530

    finished

---
+++ app/src/main/java/com/example/jon_snow/swoosh/Controller/LastActivity.kt
@@ -0,0 +1,12 @@
+package com.example.jon_snow.swoosh.Controller
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class LastActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_last)
+    }
+}
